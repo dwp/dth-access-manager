1dd65a3b899ab1e0f0b5cae866a5a4b365472f6a
ibrookes <117462019+ibrookes@users.noreply.github.com>
2024-06-25 12:03:47 +0100

    Initial commit
    GitOrigin-RevId: 6944bb59596859e603fe0881baf9f990f30660cb

--- app/routes.js
+++ app/routes.js
@@ -14,56 +14,6 @@ router.post('/choice-answer', function(request, response) {
     if (country == "dth"){
         response.redirect("/request-list")
     } else {
-        response.redirect("/request-list-ol")
-    }
-})
-
-router.post('/request-decision', function(request, response) {
-
-    var decision = request.session.data['yourDecision']
-    if (decision == "approve"){
-        response.redirect("/request-list-updated")
-    } else {
-        response.redirect("/request-list-declined")
-    }
-})
-
-router.post('/request-decision2', function(request, response) {
-
-    var decision = request.session.data['yourDecision2']
-    if (decision == "approve"){
-        response.redirect("/request-approved")
-    } else {
-        response.redirect("/request-declined")
-    }
-})
-
-router.post('/approve-choice-answer', function(request, response) {
-
-    var choice = request.session.data['approve-choice']
-    if (choice == "dth"){
-        response.redirect("/approvals-dwp/flow2/request-list")
-    } else {
-        response.redirect("/approvals-ol/flow2/request-list-ol")
-    }
-})
-
-router.post('/request-decision3', function(request, response) {
-
-    var decision = request.session.data['yourDecision3']
-    if (decision == "approve"){
-        response.redirect("/approvals-dwp/flow2/request-approved")
-    } else {
-        response.redirect("/approvals-dwp/flow2/request-declined")
-    }
-})
-
-router.post('/request-decision4', function(request, response) {
-
-    var decision = request.session.data['yourDecision4']
-    if (decision == "approve"){
-        response.redirect("/approvals-ol/flow2/one-login-approved")
-    } else {
-        response.redirect("/approvals-ol/flow2/one-login-declined")
+        response.redirect("/onelogin")
     }
 })
